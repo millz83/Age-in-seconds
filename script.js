ddbfd416ea67c19f6b32610667c9bf57aa3802f3
Mildred <mbrooklyn83@gmail.com>
2021-09-24 11:00:15 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const ageInYears = prompt('How old are you (in years)? ');
+
+const ageInSeconds = ageInYears * 365.25 * 24 * 60 * 60;
+
+
+alert(`that means you have been alive for at least ${ageInSeconds} seconds!!`);
